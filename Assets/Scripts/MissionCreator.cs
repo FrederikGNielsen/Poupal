01433ca637f2d8884b8687b17b4bb018e2632563
TaisVA <117156785+TaisVA@users.noreply.github.com>
2024-04-17 15:24:47 +0200

    Insanetingy

--- Assets/Scripts/MissionCreator.cs
+++ Assets/Scripts/MissionCreator.cs
@@ -12,7 +12,7 @@ public class MissionCreator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        LootLockerSDKManager.
+        //LootLockerSDKManager.
     }
 
     // Update is called once per frame
